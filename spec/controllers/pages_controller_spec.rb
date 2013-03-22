10a032f5676e98147f2d86f3823fe64bfda190b1
Vlad <vlad@vlad-virtual-machine.(none)>
2013-03-22 21:23:45 +0400

    done static pages

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,11 +1,23 @@
 require 'spec_helper'
 
 describe PagesController do
+  render_views
 
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
+    end
+
+    it "should have the h1 'Sample App'" do
+      get 'home'
+      response.should have_selector('h1', :content => 'Sample App')
+    end
+
+    it "should have the title 'Home'" do
+      get 'home'
+      response.should have_selector('title',
+        :content => "Sample App | Home")
     end
   end
 
@@ -13,6 +25,35 @@ describe PagesController do
     it "should be successful" do
       get 'contact'
       response.should be_success
+    end
+
+    it "should have the h1 'Contact'" do
+      get 'contact'
+      response.should have_selector('h1', :content => 'Contact')
+    end
+
+    it "should have the title 'Help'" do
+      get 'contact'
+      response.should have_selector('title',
+        :content => "Sample App | Contact")
+    end
+  end
+
+  describe "GET 'about'" do
+    it "should be successfull" do
+      get 'about'
+      response.should be_success
+    end
+
+      it "should have the h1 'About Us'" do
+      get 'about'
+      response.should have_selector('h1', :content => 'About Us')
+    end
+
+    it "should have the title 'About'" do
+      get 'about'
+      response.should have_selector('title',
+        :content => "Sample App | About")
     end
   end
 
