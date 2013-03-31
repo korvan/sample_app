3a8be47b75234d0bf10c754479d0220b923043a8
Vlad <kor123@list.ru>
2013-03-31 23:20:15 +0400

    added tests for signup page added links from other pages to new signup

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,12 +1,19 @@
 require 'spec_helper'
 
 describe UsersController do
+	render_views
+	describe "GET 'new'" do
+		it "should be successful" do
+			get 'new'
+			response.should be_success
+		end
 
-  describe "GET 'new'" do
-    it "should be successful" do
-      get 'new'
-      response.should be_success
-    end
-  end
+
+		it "should have the title 'Sign up'" do
+			get 'new'
+			response.should have_selector('title',
+				:content => " | Sign up")
+		end
+	end
 
 end
